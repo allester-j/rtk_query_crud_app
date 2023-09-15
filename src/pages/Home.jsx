0e1864739e8575e0a61e4e1adef5fe796dba57cf
Allester Jones Bukenya <israelallesterj@gmail.com>
2023-09-15 17:38:01 +0300

    Made style updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,15 @@ const Home = () => {
         <>
             <div className="container">
                 <div className="mt-5 mb-5 text-center">
-                    <h1>Redux Toolkit - RTK Query - CRUD App</h1>
+                    <h1 className="text-primary">Redux Toolkit - RTK Query - CRUD App</h1>
+                    <hr />
                 </div>
                 <div className="row mt-5 pt-5">
                     <div className="col-md-3 offset-md-3">
                         <Link to='/todos' className="text-decoration-none">
                             <div className="card text-center">
                                 <div className="card-body">
-                                    <FontAwesomeIcon icon={faList} />
+                                    <FontAwesomeIcon icon={faList} className="fa-2x text-primary" />
 
                                     <h3 className="mt-3">
                                         Todos
@@ -28,7 +29,7 @@ const Home = () => {
                         <Link to='/users' className="text-decoration-none">
                             <div className="card text-center">
                                 <div className="card-body">
-                                    <FontAwesomeIcon icon={faUsers} />
+                                    <FontAwesomeIcon icon={faUsers} className="fa-2x text-primary" />
 
                                     <h3 className="mt-3">
                                         Users
